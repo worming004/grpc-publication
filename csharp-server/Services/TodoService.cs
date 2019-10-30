4ddf58fb612961a3d0ab85de8d37b0e2c7939323
worming004 <worming@hotmail.be>
2019-10-30 20:03:56 +0100

    implement getter and clean up

--- csharp-server/Services/TodoService.cs
+++ csharp-server/Services/TodoService.cs
@@ -3,33 +3,42 @@ using System.IO;
 using Grpc.Core;
 using System.Linq;
 using System.Text;
+using TodoApi;
 
 namespace csharp_server
 {
-    public class TodoService : Todo.TodoService.TodoServiceBase
+    public class TodoService : TodoApi.TodoService.TodoServiceBase
     {
         const string FOLDER_NAME = "Todo";
-        public override Task<Todo.AddTodoResponse> AddTodo(Todo.AddTodoRequest request, ServerCallContext context)
+        public override Task<AddTodoResponse> AddTodo(AddTodoRequest request, ServerCallContext context)
         {
             File.WriteAllLines(BuildFilePath(request.Name), new[] { request.Content });
-            return Task.FromResult(new Todo.AddTodoResponse());
+            return Task.FromResult(new AddTodoResponse());
         }
 
-        public override Task<Todo.GetAllTodoResponse> GetAllTodo(Todo.GetAllTodoRequest request, ServerCallContext context) {
-            // var description = GetFileContent(requ)
+        public override Task<GetAllTodoResponse> GetAllTodo(GetAllTodoRequest request, ServerCallContext context)
+        {
+            var result = new GetAllTodoResponse();
+            result.Todo.Add(GetAllTodo());
+
+            return Task.FromResult(result);
         }
 
-        private Todo.SingleTodo[] GetAllTodo() {
+        private Todo[] GetAllTodo()
+        {
             var allFileNames = Directory.GetFiles(FOLDER_NAME);
-            return allFileNames.Select(fName => {
-                return new Todo.SingleTodo{
+            return allFileNames.Select(fName =>
+            {
+                return new Todo
+                {
                     Name = fName,
                     Content = GetFileContent(fName)
                 };
             }).ToArray();
         }
 
-        private string BuildFilePath(string fileName) {
+        private string BuildFilePath(string fileName)
+        {
             return new StringBuilder()
                         .Append(FOLDER_NAME)
                         .Append('\\')
@@ -37,7 +46,8 @@ namespace csharp_server
                         .ToString();
         }
 
-        private string GetFileContent(string todoName) {
+        private string GetFileContent(string todoName)
+        {
             return File.ReadAllText(BuildFilePath(todoName));
         }
     }
--- csharp-server/Startup.cs
+++ csharp-server/Startup.cs
@@ -31,12 +31,12 @@ namespace csharp_server
 
             app.UseEndpoints(endpoints =>
             {
-                // endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<TodoService>();
 
-                // endpoints.MapGet("/", async context =>
-                // {
-                //     await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
-                // });
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
             });
         }
     }
