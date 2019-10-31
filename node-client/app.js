512a63deef43c822cafffffcff43296f47a07b4e
worming004 <worming@hotmail.be>
2019-10-31 08:30:07 +0100

    nettoyage + segregation des commandes

--- node-client/app.js
+++ node-client/app.js
@@ -3,19 +3,28 @@ const minimist = require('minimist');
 
 var args = minimist(process.argv.slice(2));
 
-// appeler 'node app.js -c "contenu du todo" -n "nom du todo" par exemple
+// appeler 'node app.js -a -c "contenu du todo" -n "nom du todo" par exemple
 
 if (args.c)
     args.content = args.c;
 if (args.n)
     args.name = args.n;
 
-todoService.AddTodo(args, (err, resp) => {
-    console.log('ok');
-    console.log(err);
-    console.log(resp);
+if (args.h) {
+    console.log('sans argument, l\'application retourne tous les todo du serveur\r\n');
+    console.log('avec l\'argument -a, l\'application ajoute un todo de nom -n et de contenu -c');
+    console.log('exemple: \'node .\\app.js -a -n "Mon nom" -c "Mon contenu"'); 
+}
+else if (args.a) {
+
+    todoService.AddTodo(args, (err, resp) => {
+        todoService.GetAllTodo({}, (error, res) => {
+            console.log(res)
+        });
+    });
+}
+else {
     todoService.GetAllTodo({}, (error, res) => {
         console.log(res)
-    }
-    );
-});
+    });
+}
