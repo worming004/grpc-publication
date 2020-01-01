b062be08bc4527f192d9790b37775038ceb6f9bb
worming004 <worming@hotmail.be>
2020-01-01 12:07:07 +0100

    add comments

--- node-client/get-todo-service.js
+++ node-client/get-todo-service.js
@@ -1,4 +1,4 @@
-// Chemin d'accès au fichier proto
+// Chemin d'accès au fichier proto. Modifiez la valeur selon la position de votre fichier protobuffer.
 const PROTO_PATH = __dirname  + '/../proto/todoservice.proto';
 
 const grpc = require('grpc');
@@ -17,6 +17,7 @@ const packageDefinition = protoLoader.loadSync(
 
 const api = grpc.loadPackageDefinition(packageDefinition).TodoApi;
 // Pour faciliter notre démo, nous n'utiliserons pas de crédential.
+// Nous avions forcé le serveur gRPC d'utiliser le port 5001.
 const todoService = new api.TodoService('localhost:5001', grpc.credentials.createInsecure());
 
 module.exports = todoService;
